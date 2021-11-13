fe12522ef9e57170dfb799fc2c302c1ff7919431
Elisha Chirchir <elisha.java@gmail.com>
2021-11-13 21:30:09 +0300

    initial commit for e-doctor app designs

---
+++ app/src/main/java/com/dev/e_doctor/DoctorProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.dev.e_doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_profile)
+    }
+}
